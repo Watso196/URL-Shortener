339f7d20d7638a8efc04e595d888f54b34602572
Kalib <watso196@msu.edu>
2018-03-25 12:52:48 -0400

    Add HTML, CSS, and JS files for application

---
+++ url-shortener.js
@@ -0,0 +1,114 @@
+//Grab input variables
+var shortUrlInput = document.querySelector("#short-input");
+var longUrlInput = document.querySelector("#long-input");
+
+//Grab button variables for tracking button presses
+var shortenButton = document.querySelector("#long-url button");
+var lengthenButton = document.querySelector("#short-url button");
+
+
+//User presses submit button
+document.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //Determine which action the user wants to complete
+  if (e.target == shortenButton) {
+    //Check that input value is not blank
+    if (longUrlInput.value != "") {
+      createShortUrl();
+    //If blank show error message
+    } else {
+      createResponse("You haven't entered a long URL. Enter a long URL to create a short one.");
+    }
+  } else if (e.target == lengthenButton) {
+    //Check that input value is not blank
+    if (shortUrlInput.value != "") {
+      createLongUrl();
+    //If blank show error message
+    } else {
+      createResponse("You haven't entered a short URL. Enter a short URL to reveal the long one.");
+    }
+  }
+});
+
+
+//Function to create a paragraph to append to the document
+function createResponse (message) {
+  //If a message is currently on screen, remove it
+  if (document.querySelector("span")) {
+    var span = document.querySelector("span");
+    container.removeChild(span);
+  }
+
+  //Create a span element
+  var span = document.createElement("span");
+  //Set the content of the span to the message
+  span.innerHTML = message;
+  //Find the element we want to insert the message after
+  var intro = document.querySelector("#container p");
+  //Insert the span message after that element
+  intro.parentNode.insertBefore(span, intro.nextSibling);
+}
+
+
+function createShortUrl () {
+  //Create XML HTTP request
+  var shortReq = new XMLHttpRequest();
+  //Set URL for request and include API key
+  var url = "https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyBMRkeU0d9r8N42_El2L91CwNCfq-0vDyM";
+  //Open our request
+  shortReq.open("POST", url, true);
+  //Set a header for the request with our content type as JSON
+  shortReq.setRequestHeader("Content-type", "application/json");
+  //Create the callback function when the response is ready
+  shortReq.onreadystatechange = function () {
+      //If request response is ready and status is clear
+      if (shortReq.readyState === 4 && shortReq.status === 200) {
+          //Parse json data returned by application
+          var json = JSON.parse(shortReq.responseText);
+          //Create a response on the page by grabbing the id property
+          createResponse("Your new short URL:<br />" + json.id);
+      }
+  };
+  //Stringify the data so it can be passed as JSON
+  var data = JSON.stringify({"longUrl": longUrlInput.value}); //grab input value here
+  //Send XML HTTP request with above data
+  shortReq.send(data);
+}
+
+
+function createLongUrl () {
+  //Create XML HTTP request
+  var longReq = new XMLHttpRequest();
+  //Set URL for request and include API key
+  var url = "https://www.googleapis.com/urlshortener/v1/"
+  + "url?key=AIzaSyBMRkeU0d9r8N42_El2L91CwNCfq-0vDyM"
+  //add ShortUrl to GET request
+  + "&shortUrl=" + shortUrlInput.value; //grab input value
+  //Open our request - GET this time
+  longReq.open("GET", url, true);
+  //Set a header for the request with our content type as JSON
+  //Does a GET request need a request header?
+  longReq.setRequestHeader("Content-type", "text/plain");
+  //Create the callback function when the response is ready
+  longReq.onreadystatechange = function () {
+      //Parse json data returned by application
+      var json = JSON.parse(longReq.responseText);
+
+      //If request response is ready and status is clear
+      if (longReq.readyState === 4) {
+
+        if (longReq.status === 200) {
+          //Create a response on the page by grabbing the id property
+          createResponse("The lengthened URL:<br />" + json.longUrl);
+        } else {
+          console.log(json);
+          createResponse("There was an error: " + json.error.code + " " +
+          json.error.message);
+        }
+
+      }
+  };
+  //Send XML HTTP request
+  longReq.send();
+}
